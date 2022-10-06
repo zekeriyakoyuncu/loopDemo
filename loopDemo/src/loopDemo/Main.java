c69d71f19e3b32a5980b70c41dd0ff810afc99d0
zeker <zeker@MSI>
2022-10-06 06:57:44 +0300

    Added while loop.

--- loopDemo/src/loopDemo/Main.java
+++ loopDemo/src/loopDemo/Main.java
@@ -8,6 +8,14 @@ public class Main {
 			System.out.println(i);
 		}
 		System.out.println("Döngü bitti.");
+		
+		//While
+		int i = 2;
+		while(i<10) {
+			System.out.println(i);
+			i+=2;
+		}
+		System.out.println("Döngü bitti.");
 	}
 
 }
